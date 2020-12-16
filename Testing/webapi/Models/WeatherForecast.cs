4e6a572a904131902d57cd23b2f24f84b4b8e215
Gerardo Herrera Gonzalez <gherreragonzalez1@buffs.wtamu.edu>
2020-12-16 00:43:32 -0600

    added Testing project

---
+++ Testing/webapi/Models/WeatherForecast.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace webapi.Models
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public int TemperatureK => 0 + (int)(TemperatureC * 273.15);
+
+        public string Summary { get; set; }
+    }
+}
